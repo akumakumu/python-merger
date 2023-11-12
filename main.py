7e5ac159b82cf3d18afe71607d757782b72282de
kuu <akumakumu@gmail.com>
2023-11-12 19:50:58 +0700

    base

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os
+import glob
+
+xlsx_files = glob.glob('*.{}'.format('xlsx'))
+
+df_concat = pd.concat([pd.read_excel(f) for f in xlsx_files ], ignore_index=True)
+
+df_concat.to_excel(r'bin/result.xlsx', index=False)
